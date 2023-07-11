466c0ae2723b334dedd3a6a51e47a9975e1c2f46
Federico Stempler <zackmango@gmail.com>
2023-07-10 22:15:14 -0300

    NavBar Ok / cartWidget OK / ItemListContainer OK / Fix OK

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-transparent" data-bs-theme="dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#"><img className="logo" src="/src/assets/logo-removebg-preview.png"/></a>
+        <a className="navbar-brand" href="#"><img className="logo" src="./src/assets/logo-removebg-preview.png"/></a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
