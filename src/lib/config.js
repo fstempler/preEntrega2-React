4f4249bff37e5d36a68ed67479eed13a27a96bde
Federico Stempler <zackmango@gmail.com>
2023-08-09 14:31:36 -0300

    ItemCount OK / Cart OK - Styles OK  / Loader OK / Firebase OK / Style Fix NO

--- src/lib/config.js
+++ src/lib/config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBayA_Wj-m1Hd75KX7Ojdvg9C2Nkd7bn5A",
-  authDomain: "spincity-63560.firebaseapp.com",
-  projectId: "spincity-63560",
-  storageBucket: "spincity-63560.appspot.com",
-  messagingSenderId: "665332629305",
-  appId: "1:665332629305:web:ad04fbca6c86c3e39232e8"
+  apiKey: import.meta.env.VITE_FBS_API_KEY,
+  authDomain: import.meta.env.VITE_FBS_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FBS_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FBS_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FBS_SENDER_ID,
+  appId: import.meta.env.VITE_FBS_APP_ID,
 };
 
 
